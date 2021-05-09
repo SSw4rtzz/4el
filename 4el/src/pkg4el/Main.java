bb08c663fe5d1bf6669b6ff9dce7810477d3f473
Sw4rtz <16618607+lSw4rtzl@users.noreply.github.com>
2021-05-09 20:16:15 +0100

    Co-authored-by: Rebolas <Rebolas@users.noreply.github.com>

--- 4el/src/pkg4el/Main.java
+++ 4el/src/pkg4el/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pkg4el;
 
 import javafx.application.Application;
@@ -11,12 +6,29 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-/**
- *
- * @author rubensw4rtz
- */
-public class Main extends Application {
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                 *
+ *                 444444444                     lllllll           *
+ *                4::::::::4                     l:::::l           *
+ *               4:::::::::4                     l:::::l           *
+ *              4::::44::::4                     l:::::l           *
+ *             4::::4 4::::4      eeeeeeeeeeee    l::::l           *
+ *            4::::4  4::::4    ee::::::::::::ee  l::::l           *
+ *           4::::4   4::::4   e::::::eeeee:::::eel::::l           *
+ *          4::::444444::::444e::::::e     e:::::el::::l           *
+ *          4::::::::::::::::4e:::::::eeeee::::::el::::l           *
+ *          4444444444:::::444e:::::::::::::::::e l::::l           *
+ *                    4::::4  e::::::eeeeeeeeeee  l::::l           *
+ *                    4::::4  e:::::::e           l::::l           *
+ *                    4::::4  e::::::::e         l::::::l          *
+ *                  44::::::44 e::::::::eeeeeeee l::::::l          *
+ *                  4::::::::4  ee:::::::::::::e l::::::l          *
+ *                  4444444444    eeeeeeeeeeeeee llllllll          *
+ *             @author Ruben Anastácio & Tiago Carvalho            *
+ *                                                                 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
             Button btn1 = new Button("Olá mundinho");
