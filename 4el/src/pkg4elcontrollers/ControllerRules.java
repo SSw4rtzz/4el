5a260978d878939f65ca31c5419a274e2810d6b8
Rebolas <tiagocarvalho6900@gmail.com>
2021-06-11 18:16:57 +0100

    nome

--- 4el/src/pkg4elcontrollers/ControllerRules.java
+++ 4el/src/pkg4elcontrollers/ControllerRules.java
@@ -7,8 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
+
 import javafx.stage.Stage;
 
 public class ControllerRules {
